c5c9a9c5bbafcbcbe861a6e3369160fed51ce99c
Azamat S. Kalimoulline <turtle@bazon.ru>
2012-04-16 14:00:25 +0400

    Change in test.

--- test.lisp
+++ test.lisp
@@ -3,7 +3,7 @@
 ;(defvar *pool* (make-instance 'thread-pool :name "test" :min-size 2))
 ;(defvar *pool* (make-instance 'thread-pool :name "test" :min-size 2 :max-size nil))
 (defvar *pool* (make-instance 'thread-pool :name "test" :min-size 2 :max-size 3))
-;(defvar *pool* (make-instance 'thread-pool :name "test" :min-size 2 :max-size 200))
+;(defvar *pool* (make-instance 'thread-pool :name "test" :min-size 2 :max-size 201))
 (start-pool *pool*)
 (execute *pool*
          #'(lambda ()
