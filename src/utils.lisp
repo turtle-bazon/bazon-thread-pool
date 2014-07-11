65bd38db173d3b7bb8b34ed446e545be9d431e2f
Azamat S. Kalimoulline <turtle@bazon.ru>
2014-07-12 01:29:16 +0400

    Blocking queue implementation.

--- src/utils.lisp
+++ src/utils.lisp
@@ -47,50 +47,125 @@
   (:documentation ""))
 
 ;;;;;;; A simple, but efficient, queue implementation, by Paul Graham, ANSI Common Lisp
-;;;;;;; Modified to be synchronized at simultaneous access
 
-(defclass synchronized-queue (synchronized-collection)
-  ((queue
-    :initform (cons nil nil))))
+(defclass queue ()
+  ((_queue
+    :initform (cons nil nil))
+   (count
+    :initform 0)))
 
-(defgeneric make-synchronized-queue ()
+(defgeneric enqueue (queue object)
   (:documentation ""))
 
-(defgeneric enqueue (object synchronized-queue)
+(defgeneric dequeue (queue)
   (:documentation ""))
 
-(defgeneric dequeue (synchronized-queue)
-  (:documentation ""))
-
-(defgeneric peek-queue (synchronized-queue)
+(defgeneric size (queue)
   (:documentation ""))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod make-synchronized-queue ()
+(defun make-queue ()
+  (make-instance 'queue))
+
+(defmethod enqueue ((queue queue) object)
+  (with-slots (_queue count)
+      queue
+    (if (null (car _queue))
+	(setf (cdr _queue) (setf (car _queue) (cons object nil)))
+	(setf (cdr (cdr _queue)) (cons object nil)
+	      (cdr _queue) (cdr (cdr _queue))))
+    (incf count)
+    (car _queue)))
+
+(defmethod dequeue ((queue queue))
+  (with-slots (_queue count)
+      queue
+    (let ((queue-head (pop (car _queue))))
+      (when (> count 0)
+	(decf count))
+      queue-head)))
+
+(defmethod size ((queue queue))
+  (with-slots (count)
+      queue
+    count))
+
+;;;;;;; Blocking queue that blocks current thread on enqueue and dequeue (see docs)
+
+(defclass blocking-queue (queue)
+  ((max-size
+    :initform nil
+    :initarg :max-size)
+   (internal-queue
+    :initform (make-queue))
+   (lock
+    :initform (make-lock))
+   (not-empty-condition
+    :initform (make-condition-variable :name "not-empty-condition"))
+   (not-full-condition
+    :initform (make-condition-variable :name "not-full-condition"))))
+
+(defun make-blocking-queue (&optional queue-max-size)
+  (make-instance 'blocking-queue :max-size queue-max-size))
+
+(defmethod enqueue ((blocking-queue blocking-queue) object)
+  (with-slots (max-size internal-queue lock not-empty-condition not-full-condition)
+      blocking-queue
+    (with-lock-held (lock)
+      (when max-size
+	(iter (while (= (size internal-queue) max-size))
+	      (condition-wait not-full-condition lock)))
+      (let ((enqueue-result (enqueue internal-queue object)))
+	(condition-notify not-empty-condition)
+	enqueue-result))))
+
+(defmethod dequeue ((blocking-queue blocking-queue))
+  (with-slots (internal-queue lock not-empty-condition not-full-condition)
+      blocking-queue
+    (with-lock-held (lock)
+      (iter (while (= (size internal-queue) 0))
+	    (condition-wait not-empty-condition lock))
+      (let ((dequeue-result (dequeue internal-queue)))
+	(condition-notify not-full-condition)
+	dequeue-result))))
+
+(defmethod size ((blocking-queue blocking-queue))
+  (with-slots (internal-queue lock)
+      blocking-queue
+    (with-lock-held (lock)
+      (size internal-queue))))
+
+;;;;;;; Modified to be synchronized at simultaneous access
+
+(defclass synchronized-queue (queue)
+  ((internal-queue
+    :initform (make-queue))
+   (lock
+    :initform (make-lock))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun make-synchronized-queue ()
   (make-instance 'synchronized-queue))
 
 (defmethod enqueue ((synchronized-queue synchronized-queue) object)
-  (with-slots (queue lock)
+  (with-slots (internal-queue lock)
       synchronized-queue
     (with-lock-held (lock)
-      (if (null (car queue))
-	  (setf (cdr queue) (setf (car queue) (cons object nil)))
-	  (setf (cdr (cdr queue)) (cons object nil)
-		(cdr queue) (cdr (cdr queue))))
-      (car queue))))
+      (enqueue internal-queue object))))
 
 (defmethod dequeue ((synchronized-queue synchronized-queue))
-  (with-slots (queue lock)
+  (with-slots (internal-queue lock)
       synchronized-queue
     (with-lock-held (lock)
-      (pop (car queue)))))
+      (dequeue internal-queue))))
 
-(defmethod peek-queue ((synchronized-queue synchronized-queue))
-  (car (car (slot-value synchronized-queue 'queue))))
-
-(defmethod empty-p ((synchronized-queue synchronized-queue))
-  (null (car (slot-value synchronized-queue 'queue))))
+(defmethod size ((synchronized-queue synchronized-queue))
+  (with-slots (internal-queue lock)
+      synchronized-queue
+    (with-lock-held (lock)
+      (size internal-queue))))
 
 ;;;;;;; Synchronized hashset
 
